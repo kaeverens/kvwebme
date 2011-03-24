c125d37eb0ca3265610808885d2fa6dbf04af799
kae.verens <kae.verens@a0706164-33d4-2dc3-03d6-3f270faf84b9>
2011-03-24 19:30:19 +0000

    FB auth should return a string - not just echi it out

--- ww.plugins/privacy/frontend/widget-login.php
+++ ww.plugins/privacy/frontend/widget-login.php
@@ -1,12 +1,12 @@
 <?php
-echo '<div id="userauthentication-widget" widget-id="'
+$c='<div id="userauthentication-widget" widget-id="'
 	.$widget_id.'-'.$vars->id.'">'
 	.'log in with <button>email and password</button>';
 if (isset($vars->external_login)
 	&& $vars->external_login=='1'
 ) {
-	echo ' or '
+	$c.=' or '
 		.'<img src="/ww.plugins/privacy/i/facebook.png" appid="'.$vars->fbappid
 		.'" class="facebook" alt="Facebook"/>';
 }
-echo '</div>';
+$c.='</div>';
