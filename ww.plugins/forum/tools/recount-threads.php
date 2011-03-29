9380a326436acf7f6b6e0dc47911032e37308683
kae.verens <kae.verens@a0706164-33d4-2dc3-03d6-3f270faf84b9>
2011-03-29 08:18:47 +0000

    add recount tool. for recounting posts in a thread

---
+++ ww.plugins/forum/tools/recount-threads.php
@@ -0,0 +1,24 @@
+<?php
+/**
+  * recount the threads in each forum
+  *
+  * PHP Version 5
+  *
+  * @category   Whatever
+  * @package    WebME
+  * @subpackage Forum
+  * @author     Kae Verens <kae@kvsites.ie>
+  * @license    GPL Version 2
+  * @link       www.kvsites.ie
+ */
+
+require $_SERVER['DOCUMENT_ROOT'].'/ww.incs/basics.php';
+if (!isset($_SESSION['userdata']) || !$_SESSION['userdata']['id']) {
+	exit;
+}
+
+dbQuery(
+	'update forums_threads set num_posts='
+	.'(select count(id) as ids from forums_posts '
+	.'where thread_id=forums_threads.id)'
+);
