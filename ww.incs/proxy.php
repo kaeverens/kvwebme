1de4c89401e69057212d0421917f7a733def3d9a
conormacaoidh@gmail.com <conormacaoidh@gmail.com@a0706164-33d4-2dc3-03d6-3f270faf84b9>
2011-05-20 10:22:37 +0000

    updating the proxy file

---
+++ ww.incs/proxy.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * ww.incs/proxy.php, KV-Webme
+ *
+ * works as a proxy for accessing other web
+ * servers, only works when refered to internally
+ *
+ * the querystring is parsed so if a url like this is entered:
+ *
+ * http://kvweb.me/ww.plugins/themes-api/api.php?recent=true&count=3&start=0
+ *
+ * then all parameters will be passed to the loaded url
+ *
+ * url 		-	the url to load
+ *
+ * @author     Conor Mac Aoidh <conormacaoidh@gmail.com>
+ * @license    GPL 2.0
+ * @version    1.0
+ */
+
+require 'basics.php';
+require SCRIPTBASE . 'ww.admin/admin_libs.php';
+
+$url = addslashes( @$_GET [ 'url' ] );
+
+/**
+ * make sure host and referer are the same
+ */
+$referer = @$_SERVER[ 'HTTP_REFERER' ];
+$referer = preg_replace( '/^http:\/\//', '', $referer );
+$referer = explode( '/', $referer );
+$referer = $referer[ 0 ];
+$host = $_SERVER[ 'SERVER_NAME' ];
+
+if( $host != $referer )
+	exit;
+
+/**
+ * get rest of query string and pass to url
+ */
+if( strpos( $url, '?' ) !== 0 ){
+	$querystring = $_SERVER[ 'QUERY_STRING' ];
+	$querystring = '?' . end( explode( '?', $querystring ) );
+	$url = reset( explode( '?', $url ) ) . $querystring;
+}
+
+$content = curl( $url );
+
+echo $content;
+?>
