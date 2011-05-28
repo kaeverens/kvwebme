fa836bd46f8ffbbee05e2d3758613f67b4f175a5
kae.verens <kae.verens@a0706164-33d4-2dc3-03d6-3f270faf84b9>
2011-05-28 16:38:40 +0000

    fix bbcode links

--- ww.incs/bb2html.php
+++ ww.incs/bb2html.php
@@ -11,32 +11,39 @@
 
 function bb2html($text)
 {
-  $bbcode = array("<", ">",
-                "[list]", "[*]", "[/list]", 
-                "[img]", "[/img]", 
-                "[b]", "[/b]", 
-                "[u]", "[/u]", 
-                "[i]", "[/i]",
-                '[color="', "[/color]",
-                "[size=\"", "[/size]",
-                '[url="', "[/url]",
-                "[mail=\"", "[/mail]",
-                "[code]", "[/code]",
-                "[quote]", "[/quote]",
-                '"]');
-  $htmlcode = array("&lt;", "&gt;",
-                "<ul>", "<li>", "</ul>", 
-                "<img src=\"", "\">", 
-                "<b>", "</b>", 
-                "<u>", "</u>", 
-                "<i>", "</i>",
-                "<span style=\"color:", "</span>",
-                "<span style=\"font-size:", "</span>",
-                '<a href="', "</a>",
-                "<a href=\"mailto:", "</a>",
-                "<code>", "</code>",
-                "<table width=100% bgcolor=lightgray><tr><td bgcolor=white>", "</td></tr></table>",
-                '">');
+	$text=preg_replace(
+		'#\[url\]([^\[]*)\[/url]#',
+		'<a href="\1">\1</a>',
+		htmlspecialchars($text)
+	);
+  $bbcode = array(
+		"[list]", "[*]", "[/list]", 
+		"[img]", "[/img]", 
+		"[b]", "[/b]", 
+		"[u]", "[/u]", 
+		"[i]", "[/i]",
+		'[color="', "[/color]",
+		"[size=\"", "[/size]",
+		'[url="', "[/url]",
+		"[mail=\"", "[/mail]",
+		"[code]", "[/code]",
+		"[quote]", "[/quote]",
+		'"]'
+	);
+  $htmlcode = array(
+		"<ul>", "<li>", "</ul>", 
+		"<img src=\"", "\">", 
+		"<b>", "</b>", 
+		"<u>", "</u>", 
+		"<i>", "</i>",
+		"<span style=\"color:", "</span>",
+		"<span style=\"font-size:", "</span>",
+		'<a href="', "</a>",
+		"<a href=\"mailto:", "</a>",
+		"<code>", "</code>",
+		"<table width=100% bgcolor=lightgray><tr><td bgcolor=white>", "</td></tr></table>",
+		'">'
+	);
   $newtext = str_replace($bbcode, $htmlcode, $text);
   $newtext = nl2br($newtext);//second pass
   return $newtext;
