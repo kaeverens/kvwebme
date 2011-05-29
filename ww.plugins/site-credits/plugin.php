22676940b4dcaf4ce88c0e2cfbba10bce665a0d2
kae.verens <kae.verens@a0706164-33d4-2dc3-03d6-3f270faf84b9>
2011-05-29 22:24:28 +0000

    new plugin, site-credits.
    if this plugin is installed, and the "sitecredits-credits" value in $DBVARS is <0, then the front-end of the site is disabled.

---
+++ ww.plugins/site-credits/plugin.php
@@ -0,0 +1,36 @@
+<?php
+/**
+	* definition file for SiteCredits plugin
+	*
+	* PHP version 5
+	*
+	* @category None
+	* @package  None
+	* @author   Kae Verens <kae@kvsites.ie>
+	* @license  GPL 2.0
+	* @link     None
+	*/
+
+// { define $plugin
+$plugin=array(
+	'name' => 'SiteCredits',
+	'hide_from_admin'=>	true,
+	'description' => 'admins can pay for credits, which are used to manage site subscriptions',
+	'admin' => array(
+	),
+	'frontend' => array(
+	),
+	'triggers'      => array(
+		'page-object-loaded'=>'SiteCredits_isActive'
+	)
+);
+// }
+
+function SiteCredits_isActive() {
+	global $DBVARS;
+	if (@$DBVARS['sitecredits-credits']<0) {
+		echo '<p>Website Administrator attention needed.'
+			.' Please log into your control panel.</p>';
+		exit;
+	}
+}
