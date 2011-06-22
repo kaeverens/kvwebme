595d3cc05d4cac7f6a1ff6dc5a22d1e40a77fcea
kae.verens <kae.verens@a0706164-33d4-2dc3-03d6-3f270faf84b9>
2011-06-22 09:11:03 +0000

    add a link to the Products page-admin form for people that don't see the large "Products" link in the main menu

--- ww.plugins/products/admin/page-form.php
+++ ww.plugins/products/admin/page-form.php
@@ -2,6 +2,8 @@
 $kfm_do_not_save_session=true;
 require_once KFM_BASE_PATH.'/api/api.php';
 require_once KFM_BASE_PATH.'/initialise.php';
+$c.='<p>To edit products, please <a href="/ww.admin/plugin.php?_plugin='
+	.'products&amp;_page=products" target="_top">click here</a>.</p>';
 $c.= '<div class="tabs">';
 $c.= '<ul>';
 $c.= '<li><a href="#products-main">Main Details</a></li>';
