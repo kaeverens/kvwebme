475ca9f750df6a1b5c6558ae8c940209cbe0a4b5
kae.verens <kae.verens@a0706164-33d4-2dc3-03d6-3f270faf84b9>
2011-07-08 09:03:00 +0000

    conor - fixed a bug with news plugin creating news items

--- ww.plugins/news/admin/display.php
+++ ww.plugins/news/admin/display.php
@@ -3,15 +3,15 @@
         Webme News Plugin v0.1
         File: admin/display.php
         Developers:
-					Conor Mac Aoidh http://macaoidh.name/
+					Conor Mac Aoidh http://conormacaoidh.com/
 					Kae Verens      http://verens.com/
         Report Bugs:
-					conor@macaoidh.name
+					conor@kvsites.ie
 					kae@verens.com
 */
 
-$html.='<p>Click <a href="javascript:;" onclick="window.parent.pages_new('
-	.$page->id.');">here</a> to create a new news item.</p>'
+$html='<p>Click <a href="javascript:;" onclick="window.parent.pages_new('
+	.$page['id'].');">here</a> to create a new news item.</p>'
 	.'<p>This page should be displayed in <select name="page_vars[news_type]">'
 	.'<option value="0">headline</option><option value="1"';
 if (isset($vars['news_type']) && $vars['news_type']=='1') {
