b448e640c37700c19530b717421083b19adca727
kae.verens <kae.verens@a0706164-33d4-2dc3-03d6-3f270faf84b9>
2012-04-03 17:31:59 +0000

    fix bug when product zoom is applied to products that have multiple images.

--- ww.plugins/products/zoom.js
+++ ww.plugins/products/zoom.js
@@ -2,6 +2,7 @@ $(function() {
 	$.getScript('/j/featuredimagezoomer-1.51/featuredimagezoomer.js', function() {
 		$('.products-image .zoom').load(function() {
 			var src=this.src.replace(/.*\/\//, '/f/');
+			src=src.replace(/\/w=.*/, '');
 			var pos=this.className.replace(/.*zoom-pos-([^ $]*).*/, '$1');
 			$(this).addimagezoom({
 				zoomrange:[3, 3],
