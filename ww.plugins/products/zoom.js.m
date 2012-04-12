d67fdfd4de09736f221ab1b8d05f15f3402d5c64
kae.verens <kae.verens@a0706164-33d4-2dc3-03d6-3f270faf84b9>
2012-04-12 07:37:49 +0000

    fix Firefox bug in Zoom

--- ww.plugins/products/zoom.js.m
+++ ww.plugins/products/zoom.js.m
@@ -1,4 +1,3 @@
-// ./ww.plugins/products/zoom.js.m
 var a='<987642+&%'.split('')
 var b='%image%zoom%],magnifier%.replace(/%/featuredimagezoomer%function(){%\\%\'%'.split(a[a.length-1]),c='$(2$.getScript(&/j4-1.514.js&,2$(&.products-9 .8&).load(2var src=this.src6.*+/+//,&/f/&);src=src6+/w=.*/,&&);var pos=this.className6.*8-pos-([^ $]*).*/,&$1&);$(this).add98({8range:[3,37size:[300,3007pos:pos,cursorshade:true,large9:src})})})});'
 for(e in a)c=c.split(a[e]).join(b[e])
