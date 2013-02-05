119158a30d4dad16381336d9a10c2fb0b4ba185f
kae.verens <kae.verens@a0706164-33d4-2dc3-03d6-3f270faf84b9>
2013-02-05 16:27:55 +0000

    add missing files, and fix some errors in the missing files

--- ww.plugins/comments/api-admin.php
+++ ww.plugins/comments/api-admin.php
@@ -7,3 +7,33 @@ function Comments_adminCaptchasSet() {
 		dbQuery('insert into site_vars set name="comments_no_captchas", value="1"');
 	}
 }
+function Comments_adminModerate() {
+	$id = $_REQUEST['id'];
+	if (!is_numeric($id)) {
+		exit ('Invalid id');
+	}
+	$val = $_REQUEST['value'];
+	if ($val==0||$val==1) {
+		dbQuery('update comments set isvalid = '.$val.' where id = '.$id);
+		Core_cacheClear('comments');
+		return array(
+			'status'=>1,
+			'id'=>$id,
+			'value'=>$val
+		);
+	}
+	else {
+		return array(
+			'status'=>0,
+			'message'=>'Invalid Value'
+		);
+	}
+}
+function Comments_adminDelete() {
+	$id=(int)$_REQUEST['id'];
+	dbQuery('delete from comments where id='.$id);
+	return array(
+		'status'=>1,
+		'id'=>$id
+	);
+}
---
+++ ww.plugins/comments/frontend/comments-frontend.js
@@ -0,0 +1,163 @@
+$(function () {
+	$('#comment-form').validate({
+		rules:{
+			name:'required',
+			email:{
+				required:true,
+				email:true
+			},
+			site:{
+				url:true
+			},
+			comment:{
+				required:true
+			}
+		},
+		messages:{
+			site:{
+				url:'The URL should be in the form http://yoursite.domain'
+			}
+		}
+	});
+});
+$('div.comment-editable').live('mouseover',function() {
+	var id = $(this).attr('id').replace(/comment-wrapper-/,'');
+	if (document.getElementById('links-'+id)!=null) {
+		return;
+	}
+	$('div.comments- div.comments-actions').remove();
+	var mysqldatetime = $(this).attr('cdate');
+	var mysqldate = mysqldatetime.substring(8, 10);
+	var mysqlmonth = mysqldatetime.substring(5, 7);
+	var mysqlyear = mysqldatetime.substring(0, 4);
+	var mysqlhour = mysqldatetime.substring(11, 13);
+	var mysqlminute = mysqldatetime.substring(14, 16);
+	var mysqlsecond = mysqldatetime.substring(17, 19);
+	var now = new Date();
+	var commentDate 
+		= new Date(
+			mysqlyear, mysqlmonth-1, mysqldate, 
+			mysqlhour, mysqlminute, mysqlsecond
+		);
+	var nowInMilliseconds = Date.parse(now);
+	commentDate = Date.parse(commentDate);
+	var timeSinceCommentWasCreated = nowInMilliseconds-commentDate;
+	var fifteenMinutes = 15*60*1000;
+	var links = '<div id="links-'+id+'" class="comments-actions">';
+	if (timeSinceCommentWasCreated<fifteenMinutes) {
+		var comment = $(this).attr('comment');
+		links+= '<a '
+		links+= 'href="javascript:comments_frontend_start_edit'
+		+'('+id+', \''+comment+'\');">edit</a> ';
+	}
+	links+= '<a href="javascript:comments_frontend_start_delete('+id+');">';
+	links+= '[x]</a>';
+	links+= '</div>';
+	$(links).insertBefore('#comment-info-'+id);
+});
+$('div.comment-editable').live('mouseout',function(e) {
+	var id = e.target.id;
+	if(!/^comment-wrapper-[0-9]*$/.test(e.target.id)){
+		return;
+	}
+	$("#links-"+($(this).attr('id')).replace(/comment-wrapper-/,'')).remove();
+});
+function comments_check_captcha() {
+	if (window.comments_noCaptchas) {
+		return comments_check_success({status:1});
+	}
+	var correct = $('#recaptcha_challenge_field').val();
+	var response = $('#recaptcha_response_field').val();
+	$.post(
+		'/ww.plugins/comments/frontend/check-captcha.php',
+		{
+			"challenge":correct,
+			"response":response
+		},
+		comments_check_success
+	);
+}
+function comments_check_success(data) {
+	if (data.status) {
+		comments_insert_get_vals();
+	}
+	else {
+		alert('The letters you provided do not match the image');
+	}
+}
+function comments_insert_get_vals() {
+	var name = $('#comments-name-input').val();
+	var email = $('#comments-email-input').val();
+	var site = $('#comments-site-input').val();
+	var page = $('#comments-page-id').val();
+	var comment = $('#comments-comment-input').val();
+	comment = trim(comment);
+	$.post(
+		'/ww.plugins/comments/frontend/insert.php',
+		{
+			"name":name,
+			"email":email,
+			"site":site,
+			"page":page,
+			"comment":comment
+		},
+		comments_display_thank_you_message,
+		"json"
+	);
+}
+function comments_display_thank_you_message(data) {
+	if (!data.status) {
+		return alert(data.message);
+	}
+	alert(data.moderated?'Thank you - your comment has been received and will be uploaded as soon as possible':'Thank you for commenting');
+	document.location=document.location.toString();
+}
+function comments_frontend_start_delete(id) {
+	if (confirm('Are you sure you want to delete this comment?')) {
+		$.post(
+			'/ww.plugins/comments/frontend/delete.php',
+			{
+				"id":id
+			},
+			function(){
+				document.location=document.location.toString();
+			}
+		);
+	}
+}
+function comments_frontend_start_edit(id, comment) {
+	$('#comment-'+id).remove();
+	$(
+		'<div id="comment-edit"><input type="hidden" id="id" value="'+id+'" />'
+		+'<textarea id="comment-text">'+comment+'</textarea><br />'
+		+'<input type="button" value="Save" '
+		+'onclick="javascript:comments_edit_get_vals();" /></div>'
+	)
+	.insertAfter('#comment-info-'+id);
+}
+function comments_edit_get_vals() {
+	var id = $('#id').val();
+	var comment = $('#comment-text').val();
+	comment = trim(comment);
+	if (comment==null) {
+		return alert('The comment must contain some text');
+	}
+	$.post(
+		'/ww.plugins/comments/frontend/edit-comment.php',
+		{
+			"id":id,
+			"comment":comment
+		},
+		comments_frontend_update_comment,
+		"json"
+	);
+}
+function comments_frontend_update_comment(data) {
+	$('<div id="comment-'+data.id+'>'+htmlspecialchars(data.comment)+'</div>')
+	.insertBefore('#comment-edit');
+	$('#'+data.id).attr('comment', htmlspecialchars(data.comment));
+	$('#comment-edit').remove();
+}
+function trim(myString) {
+	return myString.replace(/^\s+|\s+$/g, '');
+}
